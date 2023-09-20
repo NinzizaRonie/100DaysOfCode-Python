c05dc92706fc8ea46563e6a2aa9df3b4098399e0
NinzizaRonie <114016614+NinzizaRonie@users.noreply.github.com>
2023-09-20 22:11:44 +0300

    Create day2.py

---
+++ Ronie-Ninziza/day2.py
@@ -0,0 +1,18 @@
+print("hello world!")
+
+#print() is a function that allowws you to display results of a code
+
+
+print("day 2 we are learning all about the print function")
+print("a print function is supposed to llook like this")
+print("print('what you would want to print')")
+
+# a simple code that generates for you  a band name based on your city of origin and your name.
+
+print("Welcome to the band name generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("what is your name\n")
+print("Your band name could be " + city + " " + pet)
+
+
+
